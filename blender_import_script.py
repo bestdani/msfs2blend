94522daa71558f63e2fa2dc20cde80c7e3ae0e49
bestdani <bestdani@googlemail.com>
2020-09-03 18:46:35 +0200

    combined into one file

--- blender_import_script.py
+++ blender_import_script.py
@@ -1,19 +1,95 @@
 import json
 import logging
 import pathlib
+import struct
 
 import bpy
 import bmesh
 
-import importer
-
 GLTF_FILE = 'examples/A320_NEO_LOD00.gltf'
+
+STRUCT_INDEX = struct.Struct('H')
+STRUCT_VEC2 = struct.Struct('ee')
+STRUCT_VEC3 = struct.Struct('fff')
+
+
+def sub_buffer_from_view(buffer, buffer_view) -> list:
+    start = buffer_view['byteOffset']
+    end = start + buffer_view['byteLength']
+    return buffer[start:end]
+
+
+def get_start_indices(accessor, stride) -> list:
+    try:
+        start = accessor['byteOffset']
+    except KeyError:
+        start = 0
+    count = accessor['count']
+    end = start + count * stride
+    return [i for i in range(start, end, stride)]
+
+
+def get_indices(accessor_indices, buffer_indices) -> list:
+    try:
+        start = accessor_indices['byteOffset']
+    except KeyError:
+        start = 0
+    end = start + accessor_indices['count'] * STRUCT_INDEX.size
+    return [
+        STRUCT_INDEX.unpack(buffer_indices[i:i + STRUCT_INDEX.size])[0]
+        for i in range(start, end, STRUCT_INDEX.size)
+    ]
+
+
+def read_primitive(gltf, buffer, selected):
+    attributes = selected['attributes']
+
+    accessor_pos = gltf['accessors'][attributes['POSITION']]
+    accessor_texcoord = gltf['accessors'][attributes['TEXCOORD_0']]
+    accessor_indices = gltf['accessors'][selected['indices']]
+    buffer_view_indices = gltf['bufferViews'][accessor_indices['bufferView']]
+    # TODO separate buffer views and buffers per accessor since they can
+    #  potentially be different
+    buffer_view_data = gltf['bufferViews'][accessor_pos['bufferView']]
+    buffer_indices = sub_buffer_from_view(buffer, buffer_view_indices)
+    buffer_data = sub_buffer_from_view(buffer, buffer_view_data)
+
+    indices = get_indices(accessor_indices, buffer_indices)
+
+    pos_starts = get_start_indices(
+        accessor_pos, buffer_view_data['byteStride'])
+    texcoord_starts = get_start_indices(
+        accessor_texcoord, buffer_view_data['byteStride'])
+
+    pos_values = [
+        STRUCT_VEC3.unpack(buffer_data[i:i + STRUCT_VEC3.size])
+        for i in pos_starts]
+    texcoord_values = [
+        STRUCT_VEC2.unpack(buffer_data[i:i + STRUCT_VEC2.size])
+        for i in texcoord_starts]
+
+    return indices, pos_values, texcoord_values
+
+
+def as_tris(indices, pos_values, texcoord_values):
+    triangles_indices = [
+        (indices[i], indices[i + 1], indices[i + 2])
+        for i in range(0, len(indices), 3)
+    ]
+    pos_tris = []
+    texcoord_tris = []
+    for tri_idx in triangles_indices:
+        i1, i2, i3 = tri_idx
+        pos_tris.append((pos_values[i1], pos_values[i2], pos_values[i3]))
+        texcoord_tris.append(
+            (texcoord_values[i1], texcoord_values[i2], texcoord_values[i3]))
+    return pos_tris, texcoord_tris
 
 
 def fill_mesh_data(buffer, gltf, gltf_mesh, uv, b_mesh, mat_mapping):
     idx_offset = 0
     primitives = gltf_mesh['primitives']
-    idx, pos, tc = importer.read_primitive(gltf, buffer, primitives[0])
+    idx, pos, tc = read_primitive(gltf, buffer, primitives[0])
 
     for p in pos:
         # converting to blender z up world
@@ -22,7 +98,7 @@ def fill_mesh_data(buffer, gltf, gltf_mesh, uv, b_mesh, mat_mapping):
 
     for prim_idx, primitive in enumerate(primitives[0:]):
         # TODO handle asobo primitives with different indices
-        # see skipped exception on a320 model
+        # see skipped exceptions on a320 model for example
         try:
             asobo_data = primitive['extras']['ASOBO_primitive']
         except KeyError:
