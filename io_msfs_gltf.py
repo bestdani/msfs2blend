d056c55aaddd834db2bbb2aa0099b23c7c73ebdf
bestdani <bestdani@googlemail.com>
2021-08-03 09:00:18 +0200

    version incremented

--- io_msfs_gltf.py
+++ io_msfs_gltf.py
@@ -19,7 +19,7 @@
 bl_info = {
     "name": "MSFS glTF importer",
     "author": "bestdani",
-    "version": (0, 2),
+    "version": (0, 3),
     "blender": (2, 80, 0),
     "location": "File > Import > MSFS glTF",
     "description": "Imports a glTF file with Asobo extensions from the "
